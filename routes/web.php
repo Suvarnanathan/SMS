89d3dd2c9769bb2b58383967522505c99d7594c4
suvarnanathan <47971490+Suvarnanathan@users.noreply.github.com>
2021-08-16 15:23:48 +0530

    ch

--- routes/web.php
+++ routes/web.php
@@ -15,21 +15,62 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::namespace('App\Http\Controllers')->group(function () {
-    Route::get('/', 'WelcomeController@welcome')->name('welcome');
     Route::get('/dashboard','dashboardController@index');
     Route::get('/new-member', 'Auth\RegisterController@newMember');
     Route::get('/user-home', 'UserController@home');
     Route::resource('participated-sports', 'participatedSportsController');
     Route::put('participated-sports/{id}/update', 'participatedSportsController@update');
-
-
-
+});
+/* geremykennady pakage */
+// Homepage Route
+Route::group(['middleware' => ['web', 'checkblocked']], function () {
+    Route::get('/', 'App\Http\Controllers\WelcomeController@welcome')->name('welcome');
+    Route::get('/terms', 'App\Http\Controllers\TermsController@terms')->name('terms');
 });
 
-
+// Authentication Routes
 Auth::routes();
 
+// Public Routes
+Route::group(['middleware' => ['web', 'activity', 'checkblocked']], function () {
 
+    // Activation Routes
+    Route::get('/activate', ['as' => 'activate', 'uses' => 'App\Http\Controllers\Auth\ActivateController@initial']);
+
+    Route::get('/activate/{token}', ['as' => 'authenticated.activate', 'uses' => 'App\Http\Controllers\Auth\ActivateController@activate']);
+    Route::get('/activation', ['as' => 'authenticated.activation-resend', 'uses' => 'App\Http\Controllers\Auth\ActivateController@resend']);
+    Route::get('/exceeded', ['as' => 'exceeded', 'uses' => 'App\Http\Controllers\Auth\ActivateController@exceeded']);
+
+    // Socialite Register Routes
+    Route::get('/social/redirect/{provider}', ['as' => 'social.redirect', 'uses' => 'App\Http\Controllers\Auth\SocialController@getSocialRedirect']);
+    Route::get('/social/handle/{provider}', ['as' => 'social.handle', 'uses' => 'App\Http\Controllers\Auth\SocialController@getSocialHandle']);
+
+    // Route to for user to reactivate their user deleted account.
+    Route::get('/re-activate/{token}', ['as' => 'user.reactivate', 'uses' => 'App\Http\Controllers\RestoreUserController@userReActivate']);
+});
+
+// Registered and Activated User Routes
+Route::group(['middleware' => ['auth', 'activated', 'activity', 'checkblocked']], function () {
+
+    // Activation Routes
+    Route::get('/activation-required', ['uses' => 'App\Http\Controllers\Auth\ActivateController@activationRequired'])->name('activation-required');
+    Route::get('/logout', ['uses' => 'App\Http\Controllers\Auth\LoginController@logout'])->name('logout');
+});
+
+// Registered and Activated User Routes
+// Route::group(['middleware' => ['auth', 'activated', 'activity', 'twostep', 'checkblocked']], function () {
+
+    //  Homepage Route - Redirect based on user role is in controller.
+    Route::get('/home', ['as' => 'public.home',   'uses' => 'App\Http\Controllers\UserController@index']);
+
+    // Show users profile - viewable by other users.
+    Route::get('profile/{username}', [
+        'as'   => '{username}',
+        'uses' => 'App\Http\Controllers\ProfilesController@show',
+    ]);
+// });
+// Route::get('profile/Admin','App\Http\Controllers\ProfilesController@show');
+// Registered, activated, and is current user routes.
 Route::group(['middleware' => ['auth', 'activated', 'currentUser', 'activity', 'twostep', 'checkblocked']], function () {
 
     // User Profile and Account Routes
